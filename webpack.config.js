c18ce76efcb6b9dd80e29c1af5be2214542b8f49
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-01 00:35:59 +0800

    Update webpack config file to include html and the module html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -10,4 +11,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
