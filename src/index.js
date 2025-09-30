9bb8b3ba7d2b860556f53926b2fddc7e691660a9
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-10-01 02:55:15 +0800

    Update index.js with code to include the image into the practice

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
 
+import odinImage from "./odin.png";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
+
 console.log(greeting);
